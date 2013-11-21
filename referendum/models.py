9d8bf9799676c0102982a88929819aa84a288077
Bruno Rahle <brahle@gmail.com>
2013-11-21 21:34:49 +0100

    Voteovi i gitignore

--- referendum/models.py
+++ referendum/models.py
@@ -4,7 +4,9 @@ import datetime
 class Vote(models.Model):
     facebook_id = models.BigIntegerField()
     vote = models.IntegerField()
-    date = models.DateTimeField('voting date')
+    date = models.DateTimeField('voting date', auto_now_add=True)
+    visible = models.BooleanField(default=True)
+    eligible = models.BooleanField(default=True)
 
     def __unicode__(self):
-        return self.name
+        return self.facebook_id + ': ' + self.vote
