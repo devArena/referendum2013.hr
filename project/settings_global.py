ca4489e03b7bffb2b7c2b7874543f14648f748e5
Bruno Rahle <brahle@gmail.com>
2013-11-22 21:27:17 +0100

    Trying to get location

--- project/settings_global.py
+++ project/settings_global.py
@@ -136,13 +136,21 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console' : {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'open_facebook.utils': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
         },
     }
 }
@@ -167,7 +175,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
+AUTH_USER_MODEL = 'referendum.FacebookUserWithLocation'
 
 FACEBOOK_STORE_FRIENDS = True
 
--- referendum/models.py
+++ referendum/models.py
@@ -1,5 +1,36 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_facebook.models import FacebookModel, get_user_model
+from django_facebook.utils import get_profile_model
+from open_facebook.api import *
+
+class Location(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+
+class FacebookUserWithLocation(AbstractUser, FacebookModel):
+    objects = UserManager()
+    location = models.ForeignKey(Location, blank=True, null=True, related_name='+')
+    hometown = models.ForeignKey(Location, blank=True, null=True, related_name='+')
+
+
+@receiver(post_save)
+def create_profile(sender, instance, created, **kwargs):
+    if sender == get_user_model():
+        user = instance
+        if user.facebook_id is not None:
+            query = 'SELECT hometown_location FROM user WHERE uid = {}'.format(user.facebook_id)
+            print user.access_token
+            facebook = OpenFacebook(user.access_token)
+            print facebook.fql(query)
+
 
 class Vote(models.Model):
     facebook_id = models.BigIntegerField()
