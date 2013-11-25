6e13f6254d20b3daa4d949f057e24040b669a45b
Bruno Rahle <brahle@gmail.com>
2013-11-25 22:45:30 +0100

    Popravljam staticke metode

--- project/settings_global.py
+++ project/settings_global.py
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(PROJECT_ROOT), 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -155,6 +156,10 @@ LOGGING = {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'django_facebook.settings' : {
+            'handlers': ['console'],
+            'level': 'ERROR',
         },
         'open_facebook.utils': {
             'handlers': ['console'],
