89ab38987aa2dfb873178883be48247f504e19d8
Bruno Rahle <brahle@gmail.com>
2013-11-26 20:36:31 +0100

    Various cache fixes

--- referendum/tasks.py
+++ referendum/tasks.py
@@ -1,22 +1,25 @@
 from __future__ import absolute_import
 
-from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.core.cache import cache
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
 from project.celery import app
 
 from referendum.models import ActiveVote, Vote
 
-@app.task
-def add(x, y):
-    return x + y
+def fix_votes(facebook_id):
+    all_my_active_votes = ActiveVote.objects.filter(facebook_id=facebook_id)
+    if len(all_my_active_votes) == 0:
+        return None
+    if len(all_my_active_votes) == 1:
+        return all_my_active_votes[0]
+    all_my_active_votes.all().delete()
 
-@app.task
-def mul(x, y):
-    return x * y
-
-@app.task
-def xsum(numbers):
-    return sum(numbers)
+    cache.delete('global_results')
+    selected_vote = Vote.objects.filter(facebook_id=facebook_id).order_by('-date')[0]
+    new_active_vote = ActiveVote(vote=selected_vote.vote, facebook_id=facebook_id)
+    new_active_vote.save()
+    return new_active_vote
 
 @app.task
 def save_vote(facebook_id, vote):
@@ -27,5 +30,13 @@ def save_vote(facebook_id, vote):
         av.vote = vote
     except ObjectDoesNotExist:
         av = ActiveVote(vote=vote, facebook_id=facebook_id)
+    except MultipleObjectsReturned:
+        av = fix_votes(facebook_id)
+        av.vote = vote
     av.save()
+
+    keys = []
+    keys.append('vote_{}'.format(facebook_id))
+    keys.append('global_results')
+    cache.delete_many(keys)
 
