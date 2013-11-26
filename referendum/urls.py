9ac86ee91290f32b52cbd4c5bf5f75fbf173e933
Bruno Rahle <brahle@gmail.com>
2013-11-26 19:23:36 +0100

    Fix za vieweve i refactoring

--- referendum/urls.py
+++ referendum/urls.py
@@ -6,21 +6,6 @@ from referendum import views
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^project/', include('project.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.example, name='example'),
-    #url(r'^stressTest/$', views.stressTest, name='stressTest'),
-    #url(r'^friendsStressTest/$', views.friendsStressTest, name='friendsStressTest'),
-    #url(r'^exampleStressTest/$', views.exampleStressTest, name='exampleStressTest'),
     url(r'^vote/$', views.vote, name='vote'),
-    url(r'^vote2/$', views.vote2, name='vote2'),
-    url(r'^results/$', views.results, name='results'),
-    url(r'^friends_results/$', views.friends_results, name='friends_results'),
 )
