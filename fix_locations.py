2057910218e4878495e33d6bf0d9a0174c34d64c
Bruno Rahle <brahle@gmail.com>
2013-11-27 20:02:46 +0100

    Popravci

--- fix_locations.py
+++ fix_locations.py
@@ -2,8 +2,8 @@ from referendum.models import Location
 
 def fix_croatian_locations():
     for l in Location.objects.all():
-        if l.country == 'Croatia':
-            print '{} is in {}'.format(l.name, l.fetch_county())
-        else:
-            print '-- {}'.format(l.name)
+        if l.country == 'Croatia' and l.county is None:
+            l.fetch_county(force=True)
+            print l.id
+
 
